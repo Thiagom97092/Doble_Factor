5d1b7f427b3765efebb99d8047a25a1efd86a5bd
Santiago Macias <arleymaya25@gmail.com>
2024-10-09 09:45:46 -0500

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION['authenticated'])) {
+    header('Location: login.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"><!--CSS de Bootstrap -->
+</head>
+<body>
+    <?php include '../templates/nav.php'?>
+    <h2>Bienvenido</h2>
+    <p>Esta es una aplicación creada para la </p>
+    <a href="logout.php">Cerrar Sesión</a>
+</body>
+</html>
+
