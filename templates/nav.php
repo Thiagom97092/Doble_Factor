d1ce79f45d00829379c97b0d10cbe566543c0304
Santiago Macias <arleymaya25@gmail.com>
2024-10-08 21:11:08 -0500

    Create nav.php
    creación del archivo nav.php

---
+++ templates/nav.php
@@ -0,0 +1,24 @@
+<!--Código bootstrap de un menu de navegación-->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Doble Factor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Inicio <span class="sr-only"></span></a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="register.php">Registrarse<span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">Iniciar Sesión<span class="sr-only"></span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
